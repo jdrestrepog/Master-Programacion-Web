44cc5218ac6f60ee5750f70f6f34dbad15de1a3c
Juan David Rpo <jdrestrepog@gmail.com>
2019-11-21 23:03:03 -0500

    Proyecto v3

--- Proyecto-JS/js/main.js
+++ Proyecto-JS/js/main.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
     //Slider
-    $('.bxslider').bxSlider({
-        mode: 'fade',
-        captions: true,
-        slideWidth: 600
-      });
-
+    if(window.location.href.indexOf('index') > -1){
+        $('.bxslider').bxSlider({
+            mode: 'fade',
+            captions: true,
+            slideWidth: 600
+          });
+    }
       //post
+    if(window.location.href.indexOf('index') > -1){
+
       var posts = [
           {
               title: 'Prueba titulo 1',
@@ -47,4 +50,50 @@ $(document).ready(function(){
         
             $("#posts").append(post);
       });
+    }
+      //Selector de temas
+      var theme = $("#theme");
+      $("#to-green").click(function(){
+        theme.attr("href", "css/green.css");
+      });
+      $("#to-red").click(function(){
+        theme.attr("href", "css/red.css");
+     });
+    $("#to-blue").click(function(){
+        theme.attr("href", "css/blue.css");
+    });   
+    
+    //Scroll arriba de la web
+    $('.subir').click(function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+
+        return false;
+    });
+
+    //Login falso
+    $("#login form").submit(function(){
+        var form_nane = $("#form_name").val();
+        localStorage.setItem("form_name", form_nane);
+    });
+
+    var form_name = localStorage.getItem("form_name");
+
+    if(form_name != null && form_name != "undefined"){
+        var about_parrafo = $("#about p");
+        $("#about p").html("<br><hr><strong>Bienvenido, " + form_name + "</strong>");
+        about_parrafo.append("<a href='#' id='logout'> Cerrar Sesión</a>");
+
+        $("#logout").click(function(){
+            localStorage.clear();
+            location.reload(true);
+        });
+        $("#login").hide();
+    }
+
+    if(window.location.href.indexOf('about') > -1){
+        $("#acordeon").accordion();
+    }
 });
